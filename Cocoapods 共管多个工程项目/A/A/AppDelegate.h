43f511a78b04ed757d58e7d3eeb415462e271d18
Jobs <lg295060456@gmail.com>
2021-04-19 18:56:04 +0700

    no message

---
+++ Cocoapods 共管多个工程项目/A/A/AppDelegate.h
@@ -0,0 +1,19 @@
+//
+//  AppDelegate.h
+//  A
+//
+//  Created by Jobs on 2021/4/19.
+//
+
+#import <UIKit/UIKit.h>
+#import <CoreData/CoreData.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (readonly, strong) NSPersistentCloudKitContainer *persistentContainer;
+
+- (void)saveContext;
+
+
+@end
+
---
+++ Cocoapods 共管多个工程项目/A/A/ViewController.h
@@ -0,0 +1,14 @@
+//
+//  ViewController.h
+//  A
+//
+//  Created by Jobs on 2021/4/19.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
